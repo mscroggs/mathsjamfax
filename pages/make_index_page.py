ac20e3694728bceb91d83d82316774d94bfab521
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2022-06-11 11:17:31 +0100

    Add pages from EMFFAx

---
+++ pages/make_index_page.py
@@ -0,0 +1,93 @@
+from pyfax import Page, Line, Color
+
+p = Page(100)
+
+# Draw EMFFAX logo
+line = Line()
+line.start_bg(Color.BLUE)
+line.add_text(" " * 37)
+line.start_bg(Color.BLACK)
+p.set_line(2, line)
+p.set_line(8, line)
+
+line = Line()
+line.start_bg(Color.YELLOW)
+line.add_block("""
+............xxxxxxxxx.............xxxxxx.............xxxxxxxxxxxxxxxxxxx
+............xxxxxxxxx.............xxxxxx.............xxxxxxxxxxxxxxxxxxx
+......xxxx..xxxxxxxxxxxxxx..xxxx..xxxxxxxxxx...xxx...xxxxxxxxxxxxxxxxxxx
+""", Color.BLUE, None)
+line.start_bg(Color.BLACK)
+p.set_line(3, line)
+
+line = Line()
+line.start_bg(Color.YELLOW)
+line.add_block("""
+......xxxx..x............x..xxxx..x........x..xxxxx..x..................
+......xx....x............x..xx....x........x..xx..x..x..................
+......xxxx..x..xx....xx..x..xxxx..x..xxxx..x..xxxxx..x..xx.xx...........
+""", Color.BLUE, None)
+line.start_bg(Color.BLACK)
+p.set_line(4, line)
+
+line = Line()
+line.start_bg(Color.YELLOW)
+line.add_block("""
+......xx....x..xxx..xxx..x..xx....x..xxxx..x..xxxxx..x..xx.xx...........
+......xxxx..x..xxxxxxxx..x..xx....x..xx....x..xx..x..x..xx.xx...........
+......xxxx..x..xx.xx.xx..x..xx....x..xxxx..x..xx..x..x...xxx............
+""", Color.BLUE, None)
+line.start_bg(Color.BLACK)
+p.set_line(5, line)
+
+line = Line()
+line.start_bg(Color.YELLOW)
+line.add_block("""
+............x..xx....xx..x........x..xx....x.........x..xx.xx...........
+............x..xx....xx..x........x..xx....x.........x..xx.xx...........
+............x..xx....xx..x........x..xx....x.........x..xx.xx...........
+""", Color.BLUE, None)
+line.start_bg(Color.BLACK)
+p.set_line(6, line)
+
+line = Line()
+line.start_bg(Color.BLUE)
+line.add_block("""
+.............xxxxxxxxxxxx..........xxxxxxxx...........xxxxxxxxxxxxxxxxxx
+.........xxxxxxxxxxxxxxxx......xxxxxxxxxxxx.......xxxxxxxxxxxxxxxxxxxxxx
+.........xxxxxxxxxxxxxxxx......xxxxxxxxxxxx.......xxxxxxxxxxxxxxxxxxxxxx
+""", Color.YELLOW, None)
+line.start_bg(Color.BLACK)
+p.set_line(7, line)
+
+# List pages
+index = [
+    ("NEWS", 101),
+    ("ABOUT EMFFAX", 114),
+    ("SOURCE CODE", 116),
+    ("_EMFCAMP", 120),
+    ("@EMFCAMP", 130),
+    ("MATHSTEROIDS", 140),
+    ("ROBOT ARMS", 200),
+    ("HOLIDAYS", 400),
+    ("BLUE PETER", 525),
+    ("LOTTERY", 555),
+    ("EMF SCHEDULE", 600),
+    ("NOW & NEXT", 606),
+    ("VILLAGES", 800),
+]
+
+for n, (i, j) in enumerate(zip(index[::2], index[1::2])):
+    line = Line()
+    line.start_fg(Color.YELLOW)
+    line.add_text((i[0] + " " * 14)[:14])
+    line.start_fg(Color.WHITE)
+    line.add_text(f"{i[1]}")
+    line.add_text(" ")
+    line.start_fg(Color.YELLOW)
+    line.add_text((j[0] + " " * 14)[:14])
+    line.start_fg(Color.WHITE)
+    line.add_text(f"{j[1]}")
+    p.set_line(10 + n, line)
+
+p.write()
---
+++ pages/make_info_pages.py
@@ -0,0 +1,96 @@
+from pyfax import Page, Line, Color
+
+p = Page(114)
+
+line = Line()
+line.start_double_size()
+line.add_text("About")
+line.start_fg(Color.YELLOW)
+line.add_text("MJFAX")
+p.set_line(2, line)
+
+n = p.add_wrapped_text(
+    4,
+    "MJFAX is a text-based information service inspired by "
+    "CEEFAX (see page 115). MJFAX was created by "
+    "Matthew Scroggs (@mscroggs).")
+n = p.add_wrapped_text(
+    n + 1,
+    "MJFAX uses vbit2 to encode teletext files into this "
+    "television's video input.")
+n = p.add_wrapped_text(
+    n + 1,
+    "The Python code that generates these pages can be found on "
+    "GitHub at github.com/mscroggs/mathsjamfax")
+
+p.write()
+
+p = Page(115)
+
+line = Line()
+line.start_double_size()
+line.add_text("About")
+line.start_fg(Color.YELLOW)
+line.add_text("CEEFAX")
+p.set_line(2, line)
+
+n = p.add_wrapped_text(
+    4,
+    "CEEFAX was a text-based information service provided by the BBC. "
+    "The service ran from 1974-2012 and could be accessed by pressing "
+    "the teletext button on any suitable TV.")
+
+n = p.add_wrapped_text(
+    n + 1,
+    "As well as being available at any time via the teletext button, "
+    "pages from CEEFAX were broadcast overnight on BBC One and BBC Two. "
+    "In 1997, BBC One started broadcasting rolling news instead of CEEFAX "
+    "pages. BBC Two continued to broadcast pages from CEEFAX until 2012.")
+
+p.write()
+
+p = Page(116)
+
+line = Line()
+n = p.add_wrapped_text(
+    2,
+    "MJFAX's source code is available at "
+    "https://github.com/mscroggs/mathsjamfax")
+
+p.add_block(n + 1, """
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+xx.......xx.xxxx...xx.xx.......xx
+xx.xxxxx.x..x...xxx.xx.x.xxxxx.xx
+xx.x...x.xx...xx.xxx...x.x...x.xx
+xx.x...x.x..x.x.x.x..x.x.x...x.xx
+xx.x...x.xxx.x.xx.xxxx.x.x...x.xx
+xx.xxxxx.x.x.xx.xx..x.xx.xxxxx.xx
+xx.......x.x.x.x.x.x.x.x.......xx
+xxxxxxxxxxxxxx.x.x.xx..xxxxxxxxxx
+xx.....x.....x.xx....x..x.x.x.xxx
+xx.xxx..xxx.xxxxx.xx....x...x.xxx
+xxx.xx....x.x...x.x.x.xxx.x..xxxx
+xx.xxx.xx.x...xx.x....x.....xx.xx
+xxxx..x.....x.x.x.x.xx.....xxxxxx
+xxxx..xxx..x.x.xxx.xxx..xxx.x.xxx
+xx..x.xx.xxxxxx.x.x.x.xxxxx....xx
+xxx...x.xxx.xx.x.x.x...x...xx..xx
+xxxxxx.x.x.xxx..x.x.xx.xx.xxxxxxx
+xx.xxx..xx.xxxxxxx.xx..xx...xx.xx
+xx.x..xx...x...xx.x.x.xxxxx..xxxx
+xx.xxx..xxx...xxxx.x.......xx..xx
+xx.x.xx.....x.xxx...xx........xxx
+xxxxxxxxxx.x.x..x..xx..xxx.xx.xxx
+xx.......x...xx..xx.x..x.x...xxxx
+xx.xxxxx.xx..x..xx.xxx.xxx.x...xx
+xx.x...x.x.xxx.x.....x.....x...xx
+xx.x...x.x.xxxxxx..xxx..x.xxxxxxx
+xx.x...x.x..x..xx.x...x...xx..xxx
+xx.xxxxx.x....xx.x...x..xxx..x.xx
+xx.......x.x..xxx...x....x...xxxx
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+""", Color.WHITE)
+
+p.write()
